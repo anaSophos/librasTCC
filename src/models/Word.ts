3f13a431f5f9bb2e8749b8ea4b2cf18df22a7cba
anaSophos <anapaulam410.costa2@gmail.com>
2024-09-02 14:23:32 -0300

    alteração no backend da tabela word com type para receber imagem ou videos

--- src/models/Word.ts
+++ src/models/Word.ts
@@ -1,4 +1,4 @@
-import mongoose from 'mongoose';
+/*import mongoose from 'mongoose';
 
 const { Schema } = mongoose;
 
@@ -21,6 +21,44 @@ const wordSchema = new Schema({
       src: {
         type: String,
         // auto: true,
+        required: true,
+      },
+      category: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+      },
+    },
+  ],
+});
+
+export default mongoose.model('Word', wordSchema);*/
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+
+const wordSchema = new Schema({
+  nameWord: {
+    type: String,
+    required: true,
+  },
+  wordDefinitions: [
+    {
+      _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        auto: true,
+        unique: true,
+      },
+      descriptionWordDefinition: {
+        type: String,
+        required: true,
+      },
+      src: {
+        type: String, // Pode ser base64 ou URL do Firebase Storage
+        required: true,
+      },
+      fileType: {
+        type: String,
+        //enum: ['image', 'gif', 'video'], // Define os tipos possíveis de arquivos
         required: true,
       },
       category: {
